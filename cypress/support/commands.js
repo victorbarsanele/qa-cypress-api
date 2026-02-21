12b7dc4e16b014ecb3d68489f7ec7a75f6cd10f3
Victor <victor.barsanele@gmail.com>
2026-02-21 11:22:27 -0300

    feat: add login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,20 +1,37 @@
 Cypress.Commands.add('createUser', (user) => {
     const uniqueEmail = `user_${Date.now()}@testapi.com`;
-    return cy.request({
-        method: 'POST',
-        url: '/usuarios',
-        body: {
-            nome: user.nome,
-            email: uniqueEmail,
-            password: user.password,
-            administrador: user.administrador
-        }
-    }).then((response) => {
-        return{
-            userId: response.body._id,
-            email: uniqueEmail,
-            password: user.password
-        }
-    });
- 
-})
+    return cy
+        .request({
+            method: 'POST',
+            url: '/usuarios',
+            body: {
+                nome: user.nome,
+                email: uniqueEmail,
+                password: user.password,
+                administrador: user.administrador,
+            },
+        })
+        .then((response) => {
+            return {
+                userId: response.body._id,
+                email: uniqueEmail,
+                password: user.password,
+            };
+        });
+});
+
+Cypress.Commands.add('Login', (email, password) => {
+    return cy
+        .request({
+            method: 'POST',
+            url: '/login',
+            body: {
+                email,
+                password,
+            },
+        })
+        .then((response) => {
+            // Return the token for use in subsequent requests
+            return response.body.authorization;
+        });
+});
