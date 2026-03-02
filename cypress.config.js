6c736dce12c6f63c93b4db20982132bbe30a8b66
Victor <victor.barsanele@gmail.com>
2026-03-02 19:43:34 -0300

    chore: configure mochawesome report and CI workflow

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,17 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  e2e: {
-    baseUrl: "https://serverest.dev",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    reporter: 'mochawesome',
+    reporterOptions: {
+        reportDir: 'cypress/reports/mocha',
+        overwrite: false,
+        html: false,
+        json: true,
     },
-  },
+    e2e: {
+        baseUrl: 'https://serverest.dev',
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+    },
 });
